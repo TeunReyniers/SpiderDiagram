934b5b8f26bdf8b417d1d871a78a94711a68c816
Teun Reyniers <teun.reyniers@outlook.be>
2018-10-04 12:51:30 +0200

    jsoneditor

--- src/components/EditStyle.js
+++ src/components/EditStyle.js
@@ -9,6 +9,8 @@ import { Sticky, StickyPositionType } from 'office-ui-fabric-react/lib/Sticky';
 import { PrimaryButton, Button } from 'office-ui-fabric-react/lib/Button';
 import { Group } from "./Group";
 import { ComboBoxList } from './ComboBoxList'
+import { JsonEditor as Editor } from 'jsoneditor-react';
+import 'jsoneditor-react/es/editor.min.css';
 
 export class EditStyle extends Component {
     constructor() {
@@ -18,36 +20,16 @@ export class EditStyle extends Component {
     }
 
     render() {
-        return <div style={{ margin: '0px', height: '100%', position: 'relative' }}>
-            <div style={{ width: '40%' }}>
+        return <div style={{ margin: '0px', height: '80vh', position: 'relative' }}>
+            <div style={{ width: '40%', height: '100%' }}>
                 <TextField placeholder="A name to reconize the style"
                     onGetErrorMessage={this._getErrorMessage}
                     label="Name"
                     required={true}></TextField>
-                <Group title='Text'>
-                    <Group title='Title' level={1}>
-                        <div className='flexColumns'>
-                            <TextField label='Position X'
-                                value={100}></TextField>
-                            <TextField label='Position Y'
-                                value={100}></TextField>
-                        </div>
-                        <ComboBoxList label='Alignment'
-                            items={['Left', 'Center', 'Right']}
-                            selectedItem='Left'></ComboBoxList>
-                        <TextField label='Color'></TextField>
-                        <TextField label='Font'></TextField>
-                    </Group>
-                    <Group title='Title' level={1}>
-                        <Group title='Title' level={2}>
-                            <TextField label='Position X'></TextField>
-                            <TextField label='Position Y'></TextField>
-                            <TextField label='Alignment'></TextField>
-                            <TextField label='Color'></TextField>
-                            <TextField label='Font'></TextField>
-                        </Group>
-                    </Group>
-                </Group>
+                    <Editor style={{height: '500px'}}
+                        value={{name: "hello"}}
+                        />
+                
             </div>
             <div style={{ background: 'blue' }}>
 
