07f4f9b18dc0422f77e493ddd3f0ff8052a0dad8
Teun Reyniers <teun.reyniers@hotmail.be>
2018-10-31 14:54:58 +0100

    version 0.1.7 released

--- public/electron.js
+++ public/electron.js
@@ -1,18 +1,7 @@
-var lru = require('lru-cache')({max: 256, maxAge: 250/*ms*/});
-
 var fs = require('fs');
-var origLstat = fs.lstatSync.bind(fs);
-
 // NB: The biggest offender of thrashing lstatSync is the node module system
 // itself, which we can't get into via any sane means.
-require('fs').lstatSync = function(p) {
-  let r = lru.get(p);
-  if (r) return r;
 
-  r = origLstat(p);
-  lru.set(p, r);
-  return r;
-};
 
 const electron = require('electron');
 const path = require('path');
